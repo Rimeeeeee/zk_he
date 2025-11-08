77b9be48e9ae3a48328d2f686238601dd549a90a
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-11-08 18:55:02 +0530

    fixes

---
+++ web_client/client/pkg/key_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const encrypt_vote_u32_b64: (a: number, b: number, c: number) => [number, number, number, number];
+export const extract_server_key_b64: (a: number, b: number) => [number, number, number, number];
+export const generate_keys_b64: () => [number, number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
